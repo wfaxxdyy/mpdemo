15a709e89d9bfd9fc829fe7010e7aff95d3b7251
wang fan <fan.wang02@weimob.com>
2020-07-21 23:55:46 +0800

    添加分页查询

--- src/main/java/cn/wf/mpdemo/entity/User.java
+++ src/main/java/cn/wf/mpdemo/entity/User.java
@@ -1,11 +1,19 @@
 package cn.wf.mpdemo.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("user")
 public class User {
+    @TableId("id")
     private Long id;
+    @TableField("name")
     private String name;
+    @TableField("age")
     private Integer age;
+    @TableField("email")
     private String email;
 }
--- src/test/java/cn/wf/mpdemo/MpdemoApplicationTests.java
+++ src/test/java/cn/wf/mpdemo/MpdemoApplicationTests.java
@@ -2,6 +2,10 @@ package cn.wf.mpdemo;
 
 import cn.wf.mpdemo.entity.User;
 import cn.wf.mpdemo.mapper.UserMapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 @SpringBootTest
+@Slf4j
 class MpdemoApplicationTests {
 
     @Autowired
@@ -18,6 +23,37 @@ class MpdemoApplicationTests {
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
+    }
+    @Test
+    public void testInsert() {
+        System.out.println(("----- testInsert method test ------"));
+        User user = new User();
+        user.setAge(11);
+        user.setEmail("wf3@163.com");
+        user.setName("wf3");
+        int i = userMapper.insert(user);
+        log.info(user.getId()+"");
+    }
+    @Test
+    public void testUpdate() {
+        System.out.println(("----- testUpdate method test ------"));
+        User user = new User();
+        user.setAge(12);
+        userMapper.update(user,new UpdateWrapper<User>().eq("name","wf3"));
+        log.info(user.getAge()+"");
+    }
+    @Test
+    public void testDelete() {
+        System.out.println(("----- testDelete method test ------"));
+        userMapper.delete(new UpdateWrapper<User>().and(x -> x.eq("name","wf").eq("id",7)));
+    }
+
+    @Test
+    public void testPageSelect() {
+        System.out.println(("----- testPageSelect method test ------"));
+        IPage<User> userPage = userMapper.selectPage(new Page<User>(1, 5), null);
+        List<User> userList = userPage.getRecords();
         userList.forEach(System.out::println);
     }
 
