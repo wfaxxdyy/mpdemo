c13864996d521a861bc189b5670b2aa32456ae69
wang fan <fan.wang02@weimob.com>
2020-07-23 10:07:39 +0800

    new branch

--- src/main/java/cn/wf/mpdemo/MpdemoApplication.java
+++ src/main/java/cn/wf/mpdemo/MpdemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MpdemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello!new branch!");
         SpringApplication.run(MpdemoApplication.class, args);
     }
 
